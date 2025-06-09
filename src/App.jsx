fa60819f8e38d1c6e0594e2b110a521f21bf8a1b
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-09 23:37:32 +0600

    dock added for small device

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,141 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [isActive, setActive] = useState("home");
+
+  const activenavHandler = (tab) => {
+    setActive(tab);
+  };
   return (
-    <>
-      <div className="navbar bg-base-100 shadow-sm">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+    <section>
+      <div className="bg-neutral">
+        <div className="w-11/12 mx-auto">
+          <div className="dock bg-neutral lg:hidden flex text-neutral-content">
+            <button
+              onClick={() => activenavHandler("home")}
+              className={isActive === "home" ? "dock-active" : ""}
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  fill="currentColor"
+                  strokeLinejoin="miter"
+                  strokeLinecap="butt"
+                >
+                  <polyline
+                    points="1 11 12 2 23 11"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-miterlimit="10"
+                    strokeWidth="2"
+                  ></polyline>
+                  <path
+                    d="m5,13v7c0,1.105.895,2,2,2h10c1.105,0,2-.895,2-2v-7"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="square"
+                    stroke-miterlimit="10"
+                    strokeWidth="2"
+                  ></path>
+                  <line
+                    x1="12"
+                    y1="22"
+                    x2="12"
+                    y2="18"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="square"
+                    stroke-miterlimit="10"
+                    strokeWidth="2"
+                  ></line>
+                </g>
+              </svg>
+              <span className="dock-label">Home</span>
+            </button>
+
+            <button
+              onClick={() => activenavHandler("inbox")}
+              className={isActive === "inbox" ? "dock-active" : ""}
+            >
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  fill="currentColor"
+                  strokeLinejoin="miter"
+                  strokeLinecap="butt"
+                >
+                  <polyline
+                    points="3 14 9 14 9 17 15 17 15 14 21 14"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-miterlimit="10"
+                    strokeWidth="2"
+                  ></polyline>
+                  <rect
+                    x="3"
+                    y="3"
+                    width="18"
+                    height="18"
+                    rx="2"
+                    ry="2"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="square"
+                    stroke-miterlimit="10"
+                    strokeWidth="2"
+                  ></rect>
+                </g>
+              </svg>
+              <span className="dock-label">Inbox</span>
+            </button>
+
+            <button
+              onClick={() => activenavHandler("settings")}
+              className={isActive === "settings" ? "dock-active" : ""}
+            >
+              <svg
+                className="size-[1.2em]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  fill="currentColor"
+                  strokeLinejoin="miter"
+                  strokeLinecap="butt"
+                >
+                  <circle
+                    cx="12"
+                    cy="12"
+                    r="3"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="square"
+                    stroke-miterlimit="10"
+                    strokeWidth="2"
+                  ></circle>
+                  <path
+                    d="m22,13.25v-2.5l-2.318-.966c-.167-.581-.395-1.135-.682-1.654l.954-2.318-1.768-1.768-2.318.954c-.518-.287-1.073-.515-1.654-.682l-.966-2.318h-2.5l-.966,2.318c-.581.167-1.135.395-1.654.682l-2.318-.954-1.768,1.768.954,2.318c-.287.518-.515,1.073-.682,1.654l-2.318.966v2.5l2.318.966c.167.581.395,1.135.682,1.654l-.954,2.318,1.768,1.768,2.318-.954c.518.287,1.073.515,1.654.682l.966,2.318h2.5l.966-2.318c.581-.167,1.135-.395,1.654-.682l2.318.954,1.768-1.768-.954-2.318c.287-.518.515-1.073.682-1.654l2.318-.966Z"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="square"
+                    stroke-miterlimit="10"
+                    strokeWidth="2"
+                  ></path>
+                </g>
+              </svg>
+              <span className="dock-label">Settings</span>
+            </button>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
         </div>
       </div>
-    </>
+    </section>
   );
 }
 
