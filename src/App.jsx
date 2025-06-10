356a653e3d066f96c2891980992fb0da190c371d
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-10 07:58:31 +0600

    nav changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { FiUser } from "react-icons/fi";
+import { IoIosNotificationsOutline } from "react-icons/io";
 
 function App() {
   const [isActive, setActive] = useState("home");
@@ -16,27 +17,9 @@ function App() {
             <div className="flex-1">
               <a className="btn btn-ghost text-neoncl hover:bg-transparent text-xl">WishperWall</a>
             </div>
-            <div className="flex gap-2">
-              <input
-                type="text"
-                placeholder="Search"
-                className="input input-bordered w-24 md:w-auto"
-              />
-              <div className="dropdown dropdown-end">
-                <div
-                  tabIndex={0}
-                  role="button"
-                  className="btn btn-ghost btn-circle avatar"
-                >
-                  <div className="w-10 rounded-full">
-                    <img
-                      alt="Tailwind CSS Navbar component"
-                      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                    />
-                  </div>
-                </div>
-                <p><FiUser /></p>
-              </div>
+            <div className="flex gap-3 items-center">
+              <p className="text-3xl text-white"><IoIosNotificationsOutline /></p>
+              <p className="text-3xl text-white"><FiUser /></p>
             </div>
           </div>
 
