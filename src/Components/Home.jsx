927e5420eb828d420b3abc2cbfd5a9065068b351
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-11 08:19:18 +0600

    react tabs added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -37,6 +37,39 @@ const Home = () => {
           )
         )}
       </div>
+      {/* name of each tab group should be unique */}
+      <div className="tabs tabs-border">
+        <input
+          type="radio"
+          name="my_tabs_2"
+          className="tab text-white"
+          aria-label="Tab 1"
+        />
+        <div className="tab-content border-base-300  bg-base-100 p-10">
+          Tab content 1
+        </div>
+
+        <input
+          type="radio"
+          name="my_tabs_2"
+          className="tab text-white"
+          aria-label="Tab 2"
+          defaultChecked
+        />
+        <div className="tab-content border-base-300 bg-base-100 p-10">
+          Tab content 2
+        </div>
+
+        <input
+          type="radio"
+          name="my_tabs_2"
+          className="tab"
+          aria-label="Tab 3"
+        />
+        <div className="tab-content border-base-300 bg-base-100 p-10">
+          Tab content 3
+        </div>
+      </div>
       <div className="pb-10">
         <PostCard></PostCard>
       </div>
