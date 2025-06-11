1f733f1e493da6db28949bdbec228ff803a13051
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-11 23:18:28 +0600

    fav icon added

--- src/Components/Vote.jsx
+++ src/Components/Vote.jsx
@@ -5,18 +5,18 @@ const Vote = () => {
     <div className="font-normal  mt-2 p-2">
       <div className="flex justify-center gap-1 items-center">
         <img
-                      className="w-10 h-10 object-cover rounded-xl"
-                      src={pp}
-                      alt="profilepic"
-                    />
+          className="w-10 h-10 object-cover rounded-xl"
+          src={pp}
+          alt="profilepic"
+        />
         <input
           type="text"
           name="wishper"
           className="w-full px-3 py-1 rounded-lg border-gray-400  border-2"
           id=""
           placeholder="Start a wishper, the wall listens..."
+          readOnly
         />
-        
       </div>
     </div>
   );
