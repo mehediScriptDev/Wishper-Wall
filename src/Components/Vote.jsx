e8c1f609f6aeb1f68771c0a4b291a2d91e6e0d15
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-11 22:25:22 +0600

    vote input added

--- src/Components/Vote.jsx
+++ src/Components/Vote.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import profilepice from '../images/profilepic.png'
 
 const Vote = () => {
     return (
-        <div className='font-normal'>
-            hey guys
+        <div className='font-normal bg-mainbg mt-2 p-2'>
+            <input type="text" name="wishper"  id="" />
+            <img src={profilepice} alt="profile" className="w-10 h-10 rounded-full object-cover border-neoncl border-2 mr-3" />
         </div>
     );
 };
