9a985447df79d1723b2f1b4252e6c74765d75073
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-12 22:44:41 +0600

    form added

--- src/Components/Vote.jsx
+++ src/Components/Vote.jsx
@@ -4,7 +4,8 @@ const Vote = () => {
   return (
     <div className="font-normal mb-10 mt-2 p-2">
       <div >
-       <div className="flex justify-center gap-1 ">
+       <form >
+        <div className="flex justify-center gap-1 ">
          <img
           className="w-10 h-10 object-cover rounded-xl"
           src={pp}
@@ -13,8 +14,10 @@ const Vote = () => {
         <textarea name="wishper" placeholder="Start a wishper, the wall listens..." className="border-2 bg-transparent outline-none rounded-md px-3 py-1 w-full border-gray-500 " id=""></textarea>
        </div>
         <div className="flex justify-end">
-          <button className="btn btn-sm mt-1 mr-1 bg-mainbg border-transparent text-textcl shadow-none">Post</button>
+          
+          <input type="submit" className="btn btn-sm mt-1 mr-1 bg-mainbg border-transparent text-textcl shadow-none" value="Post" />
         </div>
+       </form>
       </div>
        <div className="mt-4">
         {/* Dummy post 1 */}
