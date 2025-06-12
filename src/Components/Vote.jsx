f883c0d75c83143f14424fa880377db20e98317f
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-12 23:05:23 +0600

    need database

--- src/Components/Vote.jsx
+++ src/Components/Vote.jsx
@@ -1,20 +1,27 @@
+import { useState } from "react";
 import pp from "../images/profilepic.png";
 
 const Vote = () => {
+
+  const [post,setPost] =useState('');
   const postsubmitHandler = e =>{
     e.preventDefault();
+    const userpost = e.target.value;
+    setPost(userpost);
+
   }
   return (
     <div className="font-normal mb-10 mt-2 p-2">
       <div >
        <form onSubmit={postsubmitHandler}>
         <div className="flex justify-center gap-1 ">
+          {post&& <h1 className="text-white">{post}</h1>}
          <img
           className="w-10 h-10 object-cover rounded-xl"
           src={pp}
           alt="profilepic"
         />
-        <textarea name="wishper" placeholder="Start a wishper, the wall listens..." className="border-2 bg-transparent outline-none rounded-md px-3 py-1 w-full border-gray-500 " id=""></textarea>
+        <textarea name="wishper" required placeholder="Start a wishper, the wall listens..." className="border-2 bg-transparent outline-none rounded-md px-3 py-1 w-full border-gray-500 " id=""></textarea>
        </div>
         <div className="flex justify-end">
           
@@ -22,6 +29,7 @@ const Vote = () => {
         </div>
        </form>
       </div>
+      
        <div className="mt-4">
         {/* Dummy post 1 */}
         <div className="bg-mainbg p-3 rounded-xl mb-2">
