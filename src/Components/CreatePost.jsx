accf7eac4b2b467a708c0aee15bc92324cfda765
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-06-13 23:49:09 +0600

    create post section desgin donen for now

--- src/Components/CreatePost.jsx
+++ src/Components/CreatePost.jsx
@@ -1,6 +1,6 @@
 import { RxCross2 } from "react-icons/rx";
 import pp from "../images/profilepic.png";
-import { IoMdPaperPlane } from "react-icons/io";
+import { FaLightbulb } from "react-icons/fa6";
 
 const CreatePost = ({ setShow }) => {
   return (
@@ -27,7 +27,9 @@ const CreatePost = ({ setShow }) => {
                 src={pp}
                 alt="profilepic"
               />
-              <p className="text-xs opacity-25 border-gray-500 font-semibold border-[1px] p-1 rounded-3xl">Anonymous</p>
+              <p className="text-xs opacity-25 border-gray-500 font-semibold border-[1px] p-1 rounded-3xl">
+                Anonymous
+              </p>
             </div>
             <textarea
               name="wishper"
@@ -36,13 +38,23 @@ const CreatePost = ({ setShow }) => {
               className=" bg-transparent  focus:outline-none  transition duration-100 outline-none rounded-md px-3 py-1 w-full border-gray-500 "
               id=""
             ></textarea>
+
+            <p className="mt-1 text-xs flex items-center text-gray-500 italic">
+              <FaLightbulb className="text-neoncl" />
+              Try: “Solitude is power.”
+            </p>
+            <p className="mt-1 text-xs flex items-center text-gray-500 italic">
+              <FaLightbulb className="text-neoncl" />
+              Try: “Privacy is power.”
+            </p>
           </div>
           <div className="flex justify-end">
             <button
               type="submit"
               className="btn  btn-sm mt-2 mr-1 bg-mainbg border-transparent text-textcl shadow-none"
-              
-            >Post</button>
+            >
+              Post
+            </button>
           </div>
         </form>
       </div>
