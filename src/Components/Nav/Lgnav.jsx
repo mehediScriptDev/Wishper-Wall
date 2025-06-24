89447f8cfea44d6aefafeb4e3183519e77aa41c6
Mehedi <mehedi25122003@gmail.com>
2025-06-24 17:19:01 +0600

    react icon installed

---
+++ src/Components/Nav/Lgnav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Lgnav = () => {
+    return (
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Lgnav;
