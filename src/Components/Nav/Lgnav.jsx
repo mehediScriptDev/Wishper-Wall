14981861c3e1c8125f92574bdcec816333eb379a
Mehedi <mehedi25122003@gmail.com>
2025-06-24 17:53:47 +0600

    nav done for lg

--- src/Components/Nav/Lgnav.jsx
+++ src/Components/Nav/Lgnav.jsx
@@ -1,10 +1,18 @@
 import { IoMdHome } from "react-icons/io";
+import { HiUserGroup } from "react-icons/hi";
+import { IoMdNotifications } from "react-icons/io";
+import { FaBrain } from "react-icons/fa";
+import { FaItunesNote } from "react-icons/fa6";
 
 const Lgnav = () => {
   return (
     <div>
-      <div className="text-gray-400 text-5xl">
-        <IoMdHome />
+      <div className="text-gray-400 flex mb-3 justify-between text-5xl">
+        <IoMdHome className="text-gray-700" />
+        <HiUserGroup />
+        <IoMdNotifications />
+        <FaItunesNote />
+        <FaBrain />
       </div>
     </div>
   );
